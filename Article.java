0d41116f58206994196adfae49854b7af17c2d34
CHI-117 <clwns0000@naver.com>
2025-04-21 22:08:48 +0900

    롬북 리팩토링

--- Article.java
+++ Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
 public class Article {
     @Id
@@ -12,20 +16,6 @@ public class Article {
     private Long id;
     @Column
     private String title;
-    @Column String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+    @Column
+    String content;
 }
