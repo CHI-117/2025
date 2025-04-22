1f6e5fdf71b2b8406e98d0ab09d20a2abb9e2575
CHI-117 <clwns0000@naver.com>
2025-04-22 21:56:32 +0900

    데이터 목록 조회

--- ArticleController.java
+++ ArticleController.java
@@ -5,9 +5,14 @@ import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -33,5 +38,25 @@ public class ArticleController {
         //System.out.println(saved.toString());
         return "";
     }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id= "+id);
+        // 1. id 조회해서 데이터 겟
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환(보여주기)
+        return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model){
+        //1.  모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        //2. 모델에 데이터 등록
+        model.addAttribute("articleList",articleEntityList);
+        //3. 뷰페이지 설정
+        return "articles/index";
+    }
+
 
 }
