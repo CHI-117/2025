e3deb2810a64ef4fbff9ab464397d6f782b9d36e
CHI-117 <clwns0000@naver.com>
2025-04-23 21:20:43 +0900

    페이지 이동

--- MemberController.java
+++ MemberController.java
@@ -9,8 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Slf4j
 @Controller
@@ -35,7 +38,26 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
         //System.out.println(saved.toString());
-        return "";
+        return "redirect:/member/"+saved.getId();
     }
+    @GetMapping("/member/{id}")
+    public  String show(@PathVariable Long id, Model model){
+        //1. 아이디 조회
+        Member member=memberRepository.findById(id).orElse(null);
+        //2. 모델 데이터등록
+        model.addAttribute("member",member);
+        //3. 뷰페이지 반환
+        return "member/show";
+    }
+    @GetMapping("/member")
+    public String index(Model model){
+        //1.  모든 데이터 가져오기
+        Iterable<Member> members=memberRepository.findAll();
+        //2. 모델에 데이터 등록
+        model.addAttribute("member",members);
+        //3. 뷰페이지 설정
+        return "member/index";
+    }
+
 
 }
