a66a6fa955276dadae5845780ae7c431a65e68ce
CHI-117 <clwns0000@naver.com>
2025-04-30 20:53:40 +0900

    update 셀프체크

--- MemberController.java
+++ MemberController.java
@@ -58,6 +58,31 @@ public class MemberController {
         //3. 뷰페이지 설정
         return "member/index";
     }
+    @GetMapping("/member/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        //수정할 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        // 모델에 데이터 등록하기
+        model.addAttribute("member",memberEntity);
+        return "member/edit";
+    }
+    @PostMapping("/member/update")
+    public String update(MemberForm form){
+        log.info(form.toString());
+        //1.DTO를 엔티티로 변환
+        Member memberEntity = form.toEntity();
+        log.info(memberEntity.toString());
+        //2.엔티티를 DB에 저장
+        //2-1 DB에서 기존 데이터 가져오기
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        //2-2 기존 데이터값 갱신
+        if(target!= null){
+            memberRepository.save(memberEntity);
+        }
+        //3. 수정 결과 페이지로 리다이렉트
+        return "redirect:/member/"+memberEntity.getId();
+
+    }
 
 
 }
